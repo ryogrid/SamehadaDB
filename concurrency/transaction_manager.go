1f27c8e03a8a07153da62c0bc98a24c37e054c1c
Ryo Kanbayashi <ryo.contact@gmail.com>
2022-01-24 01:05:27 +0900

    wip: porting Transcation and TransactionManager class for implementing logging/recovery. (can't compile now) (2)

--- concurrency/transaction_manager.go
+++ concurrency/transaction_manager.go
@@ -1,117 +1,116 @@
 package concurrency
 
 type TransactinManager struct {
-	std::atomic<txn_id_t> next_txn_id{0}
-	LockManager *lock_manager_ __attribute__((__unused__))
-	LogManager *log_manager_ __attribute__((__unused__))
-	/** The global transaction latch is used for checkpointing. */
-	ReaderWriterLatch global_txn_latch
+	next_txn_id{0} atomic<TxnID>
+	// lock_manager *LockManager //__attribute__((__unused__))
+	log_manager *LogManager // __attribute__((__unused__))
+	// /** The global transaction latch is used for checkpointing. */
+	// global_txn_latch ReaderWriterLatch
 }
 
-std::unordered_map<txn_id_t, Transaction *> txn_map = {};
+txn_map : unordered_map<TxnID, *Transaction> = {}
 
-func (transaction_manager *TransactionManager) Begin(Transaction *txn) *Transaction {
-  // Acquire the global transaction latch in shared mode.
-  global_txn_latch.RLock()
+func (transaction_manager *TransactionManager) Begin(txn *Transaction) *Transaction {
+//   // Acquire the global transaction latch in shared mode.
+//   transaction_manager.global_txn_latch.RLock()
 
   if (txn == nullptr) {
-    txn = new Transaction(next_txn_id++)
+    txn := new Transaction(next_txn_id++)
   }
 
   if (enable_logging) {
     // TODO(student): Add logging here.
-    LogRecord log_record(txn->GetTransactionId(), txn->GetPrevLSN(), LogRecordType::BEGIN)
-    lsn_t lsn = log_manager_->AppendLogRecord(&log_record)
-    txn->SetPrevLSN(lsn)
+    log_record &LogRecord(txn.GetTransactionId(), txn.GetPrevLSN(), LogRecordType::BEGIN)
+    lsn := transaction_manager.log_manager.AppendLogRecord(&log_record)
+    txn.SetPrevLSN(lsn)
   }
 
-  txn_map[txn->GetTransactionId()] = txn
-  return txn;
+  txn_map[txn.GetTransactionId()] = txn
+  return txn
 }
 
-func (transaction_manager *TransactionManager) Commit(Transaction *txn) {
-  txn->SetState(TransactionState::COMMITTED);
+func (transaction_manager *TransactionManager) Commit(txn *Transaction) {
+  txn.SetState(TransactionState::COMMITTED)
 
 // TODO: (SDB) need implement
 /*
   // Perform all deletes before we commit.
-  auto write_set = txn->GetWriteSet()
-  while (!write_set->empty()) {
-    auto &item = write_set->back()
+  auto write_set = txn.GetWriteSet()
+  while (!write_set.empty()) {
+    auto &item = write_set.back()
     auto table = item.table;
     if (item.wtype == WType::DELETE) {
       // Note that this also releases the lock when holding the page latch.
-      table->ApplyDelete(item.rid, txn)
+      table.ApplyDelete(item.rid, txn)
     }
-    write_set->pop_back()
+    write_set.pop_back()
   }
-  write_set->clear()
+  write_set.clear()
 */
 
   if (enable_logging) {
     // TODO(student): add logging here
-    LogRecord log_record(txn->GetTransactionId(), txn->GetPrevLSN(), LogRecordType::COMMIT)
-    lsn_t lsn = log_manager->AppendLogRecord(&log_record)
-    txn->SetPrevLSN(lsn)
-    log_manager->Flush()
+    log_record := &LogRecord(txn.GetTransactionId(), txn.GetPrevLSN(), LogRecordType::COMMIT)
+    lsn := transaction_manager.log_manager.AppendLogRecord(&log_record)
+    txn.SetPrevLSN(lsn)
+    transaction_manager.log_manager.Flush()
   }
 
-  // Release all the locks.
-  ReleaseLocks(txn)
-  // Release the global transaction latch.
-  global_txn_latch_.RUnlock()
+//   // Release all the locks.
+//   transaction_manager.releaseLocks(txn)
+//   // Release the global transaction latch.
+//   transaction_manager.global_txn_latch.RUnlock()
 }
 
-func (transaction_manager *TransactionManager) Abort(Transaction *txn) {
-  txn->SetState(TransactionState::ABORTED);
+func (transaction_manager *TransactionManager) Abort(txn *Transaction) {
+  txn.SetState(TransactionState::ABORTED);
 
 // TODO: (SDB) need implement
 /*
   // Rollback before releasing the lock.
-  auto write_set = txn->GetWriteSet();
-  while (!write_set->empty()) {
-    auto &item = write_set->back()
+  auto write_set = txn.GetWriteSet();
+  while (!write_set.empty()) {
+    auto &item = write_set.back()
     auto table = item.table
     if (item.wtype == WType::DELETE) {
-      table->RollbackDelete(item.rid_, txn)
+      table.RollbackDelete(item.rid_, txn)
     } else if (item.wtype == WType::INSERT) {
       // Note that this also releases the lock when holding the page latch.
-      table->ApplyDelete(item.rid, txn)
+      table.ApplyDelete(item.rid, txn)
     } else if (item.wtype_ == WType::UPDATE) {
-      table->UpdateTuple(item.tuple, item.rid_, txn)
+      table.UpdateTuple(item.tuple, item.rid_, txn)
     }
-    write_set->pop_back()
+    write_set.pop_back()
   }
-  write_set->clear()
+  write_set.clear()
 */
 
   if (enable_logging) {
     // TODO(student): add logging here
-    LogRecord log_record(txn->GetTransactionId(), txn->GetPrevLSN(), LogRecordType::ABORT)
-    lsn_t lsn = log_manager_->AppendLogRecord(&log_record)
-    txn->SetPrevLSN(lsn)
+    log_record := &LogRecord(txn.GetTransactionId(), txn.GetPrevLSN(), LogRecordType::ABORT)
+    lsn := log_manager.AppendLogRecord(&log_record)
+    txn.SetPrevLSN(lsn)
   }
 
-  // Release all the locks.
-  releaseLocks(txn)
-  // Release the global transaction latch.
-  global_txn_latch.RUnlock()
+//   // Release all the locks.
+//   transaction_manager.releaseLocks(txn)
+//   // Release the global transaction latch.
+//   transaction_manager.global_txn_latch.RUnlock()
 }
 
-func (transaction_manager *TransactionManager) BlockAllTransactions() { global_txn_latch.WLock() }
+// func (transaction_manager *TransactionManager) BlockAllTransactions() { transaction_manager.global_txn_latch.WLock() }
 
-func (transaction_manager *TransactionManager) ResumeTransactions() { global_txn_latch.WUnlock() }
+// func (transaction_manager *TransactionManager) ResumeTransactions() { transaction_manager.global_txn_latch.WUnlock() }
 
-func (transaction_manager *TransactionManager) void releaseLocks(Transaction *txn) {
-    std::unordered_set<RID> lock_set;
-    for (auto item : *txn->GetExclusiveLockSet()) {
-      lock_set.emplace(item);
-    }
-    for (auto item : *txn->GetSharedLockSet()) {
-      lock_set.emplace(item);
-    }
-    for (auto locked_rid : lock_set) {
-      lock_manager->Unlock(txn, locked_rid);
-    }
-}
-
+// func (transaction_manager *TransactionManager) void releaseLocks(txn *Transaction) {
+//     var lock_set : unordered_set<RID>
+//     for (item : *txn.GetExclusiveLockSet()) {
+//       lock_set.emplace(item)
+//     }
+//     for (item : *txn.GetSharedLockSet()) {
+//       lock_set.emplace(item)
+//     }
+//     for (locked_rid : lock_set) {
+//       lock_manager.Unlock(txn, locked_rid)
+//     }
+// }
