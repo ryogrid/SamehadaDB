2b59f1db70a0cd2f9c9c928418d58285df9d0a33
Ryo Kanbayashi <ryo.contact@gmail.com>
2022-01-28 14:52:13 +0900

    wiop

--- recovery/log_record.go
+++ recovery/log_record.go
@@ -2,20 +2,22 @@ package recovery
 
 const HEADER_SIZE uint32 = 20
 
+type LogRecordType int
+
 /** The type of the log record. */
-enum class LogRecordType {
-	INVALID = 0,
-	INSERT,
-	MARKDELETE,
-	APPLYDELETE,
-	ROLLBACKDELETE,
-	UPDATE,
-	BEGIN,
-	COMMIT,
-	ABORT,
+const {
+	INVALID LogRecordType = iota
+	INSERT
+	MARKDELETE
+	APPLYDELETE
+	ROLLBACKDELETE
+	UPDATE
+	BEGIN
+	COMMIT
+	ABORT
 	/** Creating a new page in the table heap. */
-	NEWPAGE,
-};
+	NEWPAGE
+}
 
   /**
    * For every write operation on the table page, you should write ahead a corresponding log record.
@@ -49,7 +51,7 @@ type LogRecord struct {
 	lsn LSN //INVALID_LSN
 	txn_id TxnID //INVALID_TXN_ID
 	prev_lsn LSN //INVALID_LSN
-	log_record_type{LogRecordType::INVALID} LogRecordType
+	log_record_type LogRecordType // {LogRecordType::INVALID}
 
 	// case1: for delete opeartion, delete_tuple_ for UNDO opeartion
 	delete_rid RID
@@ -70,86 +72,79 @@ type LogRecord struct {
 	// friend class LogManager;
 	// friend class LogRecovery;
 
-	Nwe() *LogRecord {}
+func NewLogRecord() *LogRecord {}
 
-	// constructor for Transaction type(BEGIN/COMMIT/ABORT)
-	New(txn_id_t txn_id, lsn_t prev_lsn, LogRecordType log_record_type) *LogRecord {
-		size := HEADER_SIZE
-		txn_id := txn_id
-		prev_lsn(prev_lsn)
-		log_record_type(log_record_type)
-	}
+// constructor for Transaction type(BEGIN/COMMIT/ABORT)
+func NewLogRecord(txn_id_t txn_id, lsn_t prev_lsn, LogRecordType log_record_type) *LogRecord {
+	size := HEADER_SIZE
+	txn_id := txn_id
+	prev_lsn(prev_lsn)
+	log_record_type(log_record_type)
+}
 
-	// constructor for INSERT/DELETE type
-	New(txn_id TxnID, prev_lsn LSN, log_record_type LogRecordType, const &rid RID, const Tuple &tuple) *LogRecord {
-	  txn_id(txn_id)
-	  prev_lsn(prev_lsn)
-	  log_record_type(log_record_type)
-	  if (log_record_type == LogRecordType::INSERT) {
+// constructor for INSERT/DELETE type
+func NewLogRecord(txn_id TxnID, prev_lsn LSN, log_record_type LogRecordType, const rid *RID, const tuple *Tuple) *LogRecord {
+	txn_id(txn_id)
+	prev_lsn(prev_lsn)
+	log_record_type(log_record_type)
+	if (log_record_type == LogRecordType::INSERT) {
 		insert_rid = rid
 		insert_tuple = tuple
-	  } else {
+	} else {
 		assert(log_record_type == LogRecordType::APPLYDELETE || log_record_type == LogRecordType::MARKDELETE ||
-			   log_record_type == LogRecordType::ROLLBACKDELETE)
+				log_record_type == LogRecordType::ROLLBACKDELETE)
 		delete_rid = rid
 		delete_tuple = tuple;
-	  }
-	  // calculate log record size
-	  size = HEADER_SIZE + sizeof(RID) + sizeof(int32_t) + tuple.GetLength()
 	}
+	// calculate log record size
+	size = HEADER_SIZE + sizeof(RID) + sizeof(int32_t) + tuple.GetLength()
+}
 
-	// constructor for UPDATE type
-	LogRecord(txn_id_t txn_id, lsn_t prev_lsn, LogRecordType log_record_type, const RID &update_rid,
-			  const Tuple &old_tuple, const Tuple &new_tuple)
-		: txn_id_(txn_id),
-		  prev_lsn_(prev_lsn),
-		  log_record_type_(log_record_type),
-		  update_rid_(update_rid),
-		  old_tuple_(old_tuple),
-		  new_tuple_(new_tuple) {
-	  // calculate log record size
-	  size_ = HEADER_SIZE + sizeof(RID) + old_tuple.GetLength() + new_tuple.GetLength() + 2 * sizeof(int32_t);
-	}
+// constructor for UPDATE type
+func NewLogRecord(txn_id_t TxnID, prev_lsn LSN, log_record_type LogRecordType, const update_rid *RID,
+			const old_tuple *Tuple, const new_tuple *Tuple) *LogRecord
+{
+	txn_id(txn_id)
+	prev_lsn(prev_lsn)
+	log_record_type(log_record_type)
+	update_rid(update_rid)
+	old_tuple(old_tuple)
+	new_tuple(new_tuple)
+	// calculate log record size
+	size = HEADER_SIZE + sizeof(RID) + old_tuple.GetLength() + new_tuple.GetLength() + 2 * sizeof(int32_t)
+}
 
-	// constructor for NEWPAGE type
-	LogRecord(txn_id_t txn_id, lsn_t prev_lsn, LogRecordType log_record_type, page_id_t page_id)
-		: size_(HEADER_SIZE),
-		  txn_id_(txn_id),
-		  prev_lsn_(prev_lsn),
-		  log_record_type_(log_record_type),
-		  prev_page_id_(page_id) {
-	  // calculate log record size
-	  size_ = HEADER_SIZE + sizeof(page_id_t);
-	}
+// constructor for NEWPAGE type
+func NewLogRecord(txn_id_t txn_id, lsn_t prev_lsn, LogRecordType log_record_type, page_id_t page_id) *LogRecord
+{
+	size_(HEADER_SIZE),
+	txn_id_(txn_id),
+	prev_lsn_(prev_lsn),
+	log_record_type_(log_record_type),
+	prev_page_id_(page_id)
+	// calculate log record size
+	size = HEADER_SIZE + sizeof(page_id_t);
+}
 
-	~LogRecord() = default;
+func (log_record *LogRecord) GetDeleteRID() RID { return log_record.delete_rid }
+func (log_record *LogRecord) GetInserteTuple() Tuple { return log_record.insert_tuple }
+func (log_record *LogRecord) GetInsertRID() RID { return log_record.insert_rid }
+func (log_record *LogRecord) GetNewPageRecord() PageID { return log_record.prev_page_id }
+func (log_record *LogRecord) GetSize() uint32 { return log_record.size }
+func (log_record *LogRecord) GetLSN() LSN { return log_record.lsn }
+func (log_record *LogRecord) GetTxnId() TxnID { return log_record.txn_id }
+func (log_record *LogRecord) GetPrevLSN() LSN { return log_record.prev_lsn }
+func (log_record *LogRecord) GetLogRecordType() LogRecordType { return log_record.log_record_type }
 
-	RID &GetDeleteRID() { return delete_rid_; }
-	Tuple &GetInserteTuple() { return insert_tuple_; }
+// // For debug purpose
+// std::string ToString() const {
+// 	std::ostringstream os;
+// 	os << "Log["
+// 		<< "size:" << size_ << ", "
+// 		<< "LSN:" << lsn_ << ", "
+// 		<< "transID:" << txn_id_ << ", "
+// 		<< "prevLSN:" << prev_lsn_ << ", "
+// 		<< "LogType:" << static_cast<int>(log_record_type_) << "]";
 
-	RID &GetInsertRID() { return insert_rid_; }
-
-	page_id_t GetNewPageRecord() { return prev_page_id_; }
-
-	int32_t GetSize() { return size_; }
-
-	lsn_t GetLSN() { return lsn_; }
-	txn_id_t GetTxnId() { return txn_id_; }
-
-	lsn_t GetPrevLSN() { return prev_lsn_; }
-
-	LogRecordType &GetLogRecordType() { return log_record_type_; }
-
-	// For debug purpose
-	std::string ToString() const {
-	  std::ostringstream os;
-	  os << "Log["
-		 << "size:" << size_ << ", "
-		 << "LSN:" << lsn_ << ", "
-		 << "transID:" << txn_id_ << ", "
-		 << "prevLSN:" << prev_lsn_ << ", "
-		 << "LogType:" << static_cast<int>(log_record_type_) << "]";
-
-	  return os.str();
-	}
-
+// 	return os.str();
+// }
