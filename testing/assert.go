bf1846c71b91094f576f16154c1a2dae36f9fdce
Ryo Kanbayashi <ryo.contact@gmail.com>
2022-02-11 01:59:24 +0900

    wip: porting log recovery testcase.

--- testing/assert.go
+++ testing/assert.go
@@ -20,6 +20,15 @@ func Assert(tb testing.TB, condition bool, msg string, v ...interface{}) {
 	}
 }
 
+// Assert fails the test if the condition is false.
+func AssertFalse(tb testing.TB, condition bool, msg string, v ...interface{}) {
+	if condition {
+		_, file, line, _ := runtime.Caller(1)
+		fmt.Printf("\033[31m%s:%d: "+msg+"\033[39m\n\n", append([]interface{}{filepath.Base(file), line}, v...)...)
+		tb.FailNow()
+	}
+}
+
 // Ok fails the test if an err is not nil.
 func Ok(tb testing.TB, err error) {
 	if err != nil {
