e937b2a5b5d216038769789cc65ee9b3075b378d
Ryo Kanbayashi <ryo.contact@gmail.com>
2022-02-13 15:24:32 +0900

    ported ConstructTuple func on log_recovery_test.go .

--- recovery/recovery_test/log_recovery_test.go
+++ recovery/recovery_test/log_recovery_test.go
@@ -2,8 +2,11 @@ package log_recovery
 
 import (
 	"fmt"
+	"math"
+	"math/rand"
 	"os"
 	"testing"
+	"time"
 
 	"github.com/ryogrid/SamehadaDB/common"
 	"github.com/ryogrid/SamehadaDB/recovery"
@@ -392,54 +395,57 @@ func TestCheckpoint(t *testing.T) {
 // TODO: (SDB) need implement for testing recovery and checkpoint mechanism
 // use a fixed schema to construct a random tuple
 func ConstructTuple(schema_ *schema.Schema) *tuple.Tuple {
-	/*
-		std::vector<Value> values;
-		Value v(TypeId::INVALID);
+	var values []types.Value
 
-		auto seed = std::chrono::system_clock::now().time_since_epoch().count();
+	v := types.NewInteger(-1) //&types.Value{types.Invalid, nil, nil, nil}
 
-		std::mt19937 generator(seed);  // mt19937 is a standard mersenne_twister_engine
+	//seed = std::chrono::system_clock::now().time_since_epoch().count()
+	seed := time.Now().UnixNano()
+	rand.Seed(seed)
 
-		for (uint32_t i = 0; i < schema_->GetColumnCount(); i++) {
-		  // get type
-		  const auto &col = schema_->GetColumn(i);
-		  TypeId type = col.GetType();
-		  switch (type) {
-			case TypeId::BOOLEAN:
-			  v = Value(type, static_cast<int8_t>(generator() % 2));
-			  break;
-			case TypeId::TINYINT:
-			  v = Value(type, static_cast<int8_t>(generator()) % 1000);
-			  break;
-			case TypeId::SMALLINT:
-			  v = Value(type, static_cast<int16_t>(generator()) % 1000);
-			  break;
-			case TypeId::INTEGER:
-			  v = Value(type, static_cast<int32_t>(generator()) % 1000);
-			  break;
-			case TypeId::BIGINT:
-			  v = Value(type, static_cast<int64_t>(generator()) % 1000);
-			  break;
-			case TypeId::VARCHAR: {
-			  static const char alphanum[] =
-				  "0123456789"
-				  "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-				  "abcdefghijklmnopqrstuvwxyz";
-			  auto len = static_cast<uint32_t>(1 + generator() % 9);
-			  char s[10];
-			  for (uint32_t j = 0; j < len; ++j) {
-				s[j] = alphanum[generator() % (sizeof(alphanum) - 1)];
-			  }
-			  s[len] = 0;
-			  v = Value(type, s, len + 1, true);
-			  break;
+	col_cnt := int(schema_.GetColumnCount())
+	//std::mt19937 generator(seed)  // mt19937 is a standard mersenne_twister_engine
+	for i := 0; i < col_cnt; i++ {
+		// get type
+		col := schema_.GetColumn(uint32(i))
+		type_ := col.GetType()
+		switch type_ {
+		case types.Boolean:
+			rand_val := rand.Intn(math.MaxUint32) % 2
+			if rand_val == 0 {
+				v = types.NewBoolean(false)
+			} else {
+				v = types.NewBoolean(true)
 			}
-			default:
-			  break;
-		  }
-		  values.emplace_back(v);
+		// case types.Tinyint:
+		// 	v = Value(type_, int8(rand.Intn(math.MaxUint32)) % 1000)
+		// 	break
+		// case types.Smallint:
+		// 	v = Value(type_, int16(rand.Intn(math.MaxUint32)) % 1000)
+		// 	break
+		case types.Integer:
+			v = types.NewInteger(int32(rand.Intn(math.MaxUint32)) % 1000)
+		// case types.BigInt:
+		// 	v = Value(type_, int64(rand.Intn(math.MaxUint32)) % 1000)
+		// 	break
+		case types.Varchar:
+			alphabets :=
+				"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+			len := int(1 + rand.Intn(math.MaxUint32)%10)
+			//char s[10]
+			s := ""
+			for j := 0; j < len; j++ {
+				//s[j] = alphanum[rand.Intn(math.MaxUint32) % (sizeof(alphanum) - 1)]
+				idx := rand.Intn(53)
+				s = s + alphabets[idx:idx]
+			}
+			//s[len] = 0
+			//v = Value(type, s, len + 1, true)
+			v = types.NewVarchar(s)
+		default:
 		}
-		return Tuple(values, schema_);
-	*/
-	return tuple.NewTuple(new(page.RID), 0, []byte{})
+		values = append(values, v)
+	}
+	return tuple.NewTupleFromSchema(values, schema_)
+	//return tuple.NewTuple(new(page.RID), 0, []byte{})
 }
