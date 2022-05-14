1e3a9a4f2276e99f15e90d3d37f8b044fe29d560
Ryo Kanbayashi <ryo.contact@gmail.com>
2022-05-14 19:32:31 +0900

    wip

--- execution/expression/comparison.go
+++ execution/expression/comparison.go
@@ -38,9 +38,9 @@ func NewComparison(left Expression, right Expression, comparisonType ComparisonT
 	return ret
 }
 
-func (c *Comparison) Evaluate(tuple_ *tuple.Tuple, schema *schema.Schema) types.Value {
-	lhs := c.children[0].Evaluate(tuple_, schema)
-	rhs := c.children[1].Evaluate(tuple_, schema)
+func (c *Comparison) Evaluate(tuple_ *tuple.Tuple, schema_ *schema.Schema) types.Value {
+	lhs := c.children[0].Evaluate(tuple_, schema_)
+	rhs := c.children[1].Evaluate(tuple_, schema_)
 	return types.NewBoolean(c.performComparison(lhs, rhs))
 }
 
