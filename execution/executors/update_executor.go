2df6bb8cb8c5b55dd40703f053e92a390b0535a3
Ryo Kanbayashi <ryo.contact@gmail.com>
2022-05-20 00:45:06 +0900

    wip

--- execution/executors/update_executor.go
+++ execution/executors/update_executor.go
@@ -51,7 +51,8 @@ func (e *UpdateExecutor) Next() (*tuple.Tuple, Done, error) {
 				defer e.it.Next()
 			}
 			rid := e.it.Current().GetRID()
-			new_tuple := tuple.NewTupleFromSchema(e.plan.GetRawValues(), e.tableMetadata.Schema())
+			values := e.plan.GetRawValues()
+			new_tuple := tuple.NewTupleFromSchema(values, e.tableMetadata.Schema())
 
 			var is_updated bool = false
 			if e.plan.GetUpdateColIdxs() == nil {
@@ -59,9 +60,12 @@ func (e *UpdateExecutor) Next() (*tuple.Tuple, Done, error) {
 			} else {
 				// TODO: (SDB) need passing data range list as a argument which is respond to update target columns (UpdateExecuter::Next())
 				var data_ranges_new [][2]int = make([][2]int, 0)
-				var update_col_idxs []int = make([]int, 0)
 
-				is_updated = e.tableMetadata.Table().UpdateTuple(new_tuple, data_ranges_new, update_col_idxs, e.tableMetadata.Schema(), *rid, e.txn)
+				for _, val := range values {
+
+				}
+
+				is_updated = e.tableMetadata.Table().UpdateTuple(new_tuple, data_ranges_new, e.plan.GetUpdateColIdxs(), e.tableMetadata.Schema(), *rid, e.txn)
 			}
 
 			if !is_updated {
