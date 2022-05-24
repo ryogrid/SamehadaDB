56e7c2876240d2cdf1296f8e079c24f72d500d01
Ryo Kanbayashi <ryo.contact@gmail.com>
2022-05-25 00:40:23 +0900

    wip

--- execution/executors/orderby_executor.go
+++ execution/executors/orderby_executor.go
@@ -5,7 +5,6 @@ import (
 	"github.com/ryogrid/SamehadaDB/execution/plans"
 	"github.com/ryogrid/SamehadaDB/storage/table/schema"
 	"github.com/ryogrid/SamehadaDB/storage/tuple"
-	"github.com/ryogrid/SamehadaDB/types"
 )
 
 /**
@@ -18,6 +17,8 @@ type OrderbyExecutor struct {
 	/** The child executor whose tuples we are aggregating. */
 	child_ []Executor
 	//exprs_ []expression.Expression
+	sort_tuples_ []*tuple.Tuple
+	cur_idx_     int // target tuple index on Next method
 }
 
 /**
@@ -28,21 +29,15 @@ type OrderbyExecutor struct {
  */
 func NewOrderbyExecutor(exec_ctx *ExecutorContext, plan *plans.OrderbyPlanNode,
 	child Executor) *OrderbyExecutor {
-	return &OrderbyExecutor{exec_ctx, plan, []Executor{child}}
+	return &OrderbyExecutor{exec_ctx, plan, []Executor{child}, make([]*tuple.Tuple, 0), 0}
 }
 
 func (e *OrderbyExecutor) GetOutputSchema() *schema.Schema { return e.plan_.OutputSchema() }
 
 func (e *OrderbyExecutor) Init() {
-	//Tuple tuple
 	e.child_[0].Init()
 	child_exec := e.child_[0]
-	//output_column_cnt := int(e.GetOutputSchema().GetColumnCount())
-	//for i := 0; i < output_column_cnt; i++ {
-	//	agg_expr := e.GetOutputSchema().GetColumn(uint32(i)).GetExpr().(expression.AggregateValueExpression)
-	//	e.exprs_ = append(e.exprs_, &agg_expr)
-	//}
-	insert_call_cnt := 0
+	inserted_tuple_cnt := 0
 	for {
 		tuple_, done, err := child_exec.Next()
 		if err != nil || done {
@@ -53,28 +48,27 @@ func (e *OrderbyExecutor) Init() {
 		}
 
 		if tuple_ != nil {
-			e.aht_.InsertCombine(e.MakeKey(tuple_), e.MakeVal(tuple_))
-			insert_call_cnt++
+			e.sort_tuples_ = append(e.sort_tuples_, tuple_)
+			inserted_tuple_cnt++
 		}
 	}
-	fmt.Printf("insert_call_cnt %d\n", insert_call_cnt)
-	e.aht_iterator_ = e.aht_.Begin()
+	fmt.Printf("inserted_tuple_cnt %d\n", inserted_tuple_cnt)
 }
 
 func (e *OrderbyExecutor) Next() (*tuple.Tuple, Done, error) {
-	for !e.aht_iterator_.IsNextEnd() && e.plan_.GetHaving() != nil && !e.plan_.GetHaving().EvaluateAggregate(e.aht_iterator_.Key().Group_bys_, e.aht_iterator_.Val().Aggregates_).ToBoolean() {
-		//++aht_iterator_;
-		e.aht_iterator_.Next()
-	}
-	if e.aht_iterator_.IsEnd() {
-		return nil, true, nil
-	}
-	var values []types.Value = make([]types.Value, 0)
-	for i := 0; i < len(e.exprs_); i++ {
-		values = append(values, e.exprs_[i].EvaluateAggregate(e.aht_iterator_.Key().Group_bys_, e.aht_iterator_.Val().Aggregates_))
-	}
-	tuple_ := tuple.NewTupleFromSchema(values, e.GetOutputSchema())
-	//++aht_iterator_;
-	e.aht_iterator_.Next()
+	//for !e.aht_iterator_.IsNextEnd() && e.plan_.GetHaving() != nil && !e.plan_.GetHaving().EvaluateAggregate(e.aht_iterator_.Key().Group_bys_, e.aht_iterator_.Val().Aggregates_).ToBoolean() {
+	//	//++aht_iterator_;
+	//	e.aht_iterator_.Next()
+	//}
+	//if e.aht_iterator_.IsEnd() {
+	//	return nil, true, nil
+	//}
+	//var values []types.Value = make([]types.Value, 0)
+	//for i := 0; i < len(e.exprs_); i++ {
+	//	values = append(values, e.exprs_[i].EvaluateAggregate(e.aht_iterator_.Key().Group_bys_, e.aht_iterator_.Val().Aggregates_))
+	//}
+	//tuple_ := tuple.NewTupleFromSchema(values, e.GetOutputSchema())
+	////++aht_iterator_;
+	//e.aht_iterator_.Next()
 	return tuple_, false, nil
 }
