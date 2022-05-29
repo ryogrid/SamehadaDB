80587b00e8d3790c900613235d01c59bc8a0fa1d
Ryo Kanbayashi <ryo.contact@gmail.com>
2022-05-29 23:09:43 +0900

    added traversing sample code execution.

--- parser/parser.go
+++ parser/parser.go
@@ -7,6 +7,32 @@ import (
 	_ "github.com/pingcap/tidb/types/parser_driver"
 )
 
+type Visitor interface {
+	Enter(n ast.Node) (node ast.Node, skipChildren bool)
+	Leave(n ast.Node) (node ast.Node, ok bool)
+}
+
+type colX struct {
+	colNames []string
+}
+
+func (v *colX) Enter(in ast.Node) (ast.Node, bool) {
+	if name, ok := in.(*ast.ColumnName); ok {
+		v.colNames = append(v.colNames, name.Name.O)
+	}
+	return in, false
+}
+
+func (v *colX) Leave(in ast.Node) (ast.Node, bool) {
+	return in, true
+}
+
+func extract(rootNode *ast.StmtNode) []string {
+	v := &colX{}
+	(*rootNode).Accept(v)
+	return v.colNames
+}
+
 func parse(sql string) (*ast.StmtNode, error) {
 	p := parser.New()
 
@@ -19,10 +45,23 @@ func parse(sql string) (*ast.StmtNode, error) {
 }
 
 func TestParsing() {
-	astNode, err := parse("SELECT a, b FROM t WHERE a = daylight")
+	//astNode, err := parse("SELECT a, b FROM t WHERE a = daylight")
+	//if err != nil {
+	//	fmt.Printf("parse error: %v\n", err.Error())
+	//	return
+	//}
+	//fmt.Printf("%v\n", *astNode)
+
+	//if len(os.Args) != 2 {
+	//	fmt.Println("usage: colx 'SQL statement'")
+	//	return
+	//}
+	//sql := os.Args[1]
+	sql := "SELECT a, b FROM t WHERE a = daylight"
+	astNode, err := parse(sql)
 	if err != nil {
 		fmt.Printf("parse error: %v\n", err.Error())
 		return
 	}
-	fmt.Printf("%v\n", *astNode)
+	fmt.Printf("%v\n", extract(astNode))
 }
