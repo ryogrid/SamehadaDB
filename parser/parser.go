9f4202b2bf3d0b062b2fcb51b81fac780228a7f2
Ryo Kanbayashi <ryo.contact@gmail.com>
2022-05-30 23:02:26 +0900

    wip

--- parser/parser.go
+++ parser/parser.go
@@ -5,33 +5,95 @@ import (
 	"github.com/pingcap/parser"
 	"github.com/pingcap/parser/ast"
 	_ "github.com/pingcap/tidb/types/parser_driver"
+	driver "github.com/pingcap/tidb/types/parser_driver"
+	"github.com/ryogrid/SamehadaDB/execution/expression"
+	"github.com/ryogrid/SamehadaDB/types"
 	"reflect"
 )
+
+type QueryType int32
+
+const (
+	SELECT QueryType = iota
+	CREATE_TABLE
+	INSERT
+	DELETE
+	UPDATE
+)
+
+type PredicateExpression struct {
+	BinaryOperationType_ *expression.ComparisonType
+	LeftVal              *string
+	RightVal             *string
+}
+
+type SetExpression struct {
+	ColName_     *string
+	UpdateValue_ *types.Value
+}
+
+type ColDefExpression struct {
+	ColName_ *string
+	ColType_ *types.TypeID
+}
 
 type Visitor interface {
 	Enter(n ast.Node) (node ast.Node, skipChildren bool)
 	Leave(n ast.Node) (node ast.Node, ok bool)
 }
 
-type colX struct {
+type SimpleSQLVisitor struct {
+	QueryType_         *QueryType
+	SelectFields_      []string
+	SetExpression_     *SetExpression
+	NewTableName_      *string
+	ColDefExpressions_ []*ColDefExpression
+	OnExpressions_     []*PredicateExpression
+	FromTable_         *string
+	JoinTable_         *string
+	WhereExpressions_  []*PredicateExpression
+	// member of example code
 	colNames []string
 }
 
-func (v *colX) Enter(in ast.Node) (ast.Node, bool) {
+func (v *SimpleSQLVisitor) Enter(in ast.Node) (ast.Node, bool) {
 	//if name, ok := in.(*ast.ColumnName); ok {
 	//	v.colNames = append(v.colNames, name.Name.O)
 	//}
 	refVal := reflect.ValueOf(in) // ValueOfでreflect.Value型のオブジェクトを取得
 	fmt.Println(refVal.Type())
+
+	switch ntype := in.(type) {
+	case *ast.SelectStmt:
+		ntype.Text()
+	case *ast.CreateTableStmt:
+	case *ast.InsertStmt:
+	case *ast.DeleteStmt:
+	case *ast.UpdateStmt:
+	case *ast.FieldList:
+	case *ast.SelectField:
+	case *ast.TableRefsClause:
+	case *ast.Assignment:
+	case *ast.Join:
+	case *ast.OnCondition:
+	case *ast.TableSource:
+	case *ast.TableNameExpr:
+	case *ast.TableName:
+	case *ast.ColumnDef:
+	case *ast.ColumnNameExpr:
+	case *ast.ColumnName:
+	case *ast.BinaryOperationExpr:
+	case *driver.ValueExpr:
+	}
 	return in, false
 }
 
-func (v *colX) Leave(in ast.Node) (ast.Node, bool) {
+func (v *SimpleSQLVisitor) Leave(in ast.Node) (ast.Node, bool) {
 	return in, true
 }
 
 func extract(rootNode *ast.StmtNode) []string {
-	v := &colX{}
+	v := &SimpleSQLVisitor{}
 	(*rootNode).Accept(v)
 	return v.colNames
 }
