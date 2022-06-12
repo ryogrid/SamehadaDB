888dc3aa616c91b7fddacb299936011c81bff718
Ryo Kanbayashi <ryo.contact@gmail.com>
2022-06-12 19:45:44 +0900

    implementing unit tests of AST processing: wrote and passed TestSinglePredicateSelectQuery testcase.

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -29,6 +29,14 @@ func TestSinglePredicateSelectQuery(t *testing.T) {
 	testingpkg.SimpleAssert(t, *queryInfo.WhereExpression_.Left_.(*BinaryOpExpression).Left_.(*string) == "a")
 	testingpkg.SimpleAssert(t, queryInfo.WhereExpression_.Right_.(*BinaryOpExpression).Left_.(*types.Value).ToInteger() == 10)
 
-	//sqlStr = "SELECT a, b FROM t WHERE a > 10.5;"
-	//queryInfo = ProcessSQLStr(&sqlStr)
+	sqlStr = "SELECT a, b FROM t WHERE a > 10.5;"
+	queryInfo = ProcessSQLStr(&sqlStr)
+	testingpkg.SimpleAssert(t, *queryInfo.QueryType_ == SELECT)
+	testingpkg.SimpleAssert(t, *queryInfo.SelectFields_[0].ColName_ == "a")
+	testingpkg.SimpleAssert(t, *queryInfo.SelectFields_[1].ColName_ == "b")
+	testingpkg.SimpleAssert(t, *queryInfo.JoinTables_[0] == "t")
+	testingpkg.SimpleAssert(t, queryInfo.WhereExpression_.ComparisonOperationType_ == expression.GreaterThan)
+	testingpkg.SimpleAssert(t, queryInfo.WhereExpression_.LogicalOperationType_ == -1)
+	testingpkg.SimpleAssert(t, *queryInfo.WhereExpression_.Left_.(*BinaryOpExpression).Left_.(*string) == "a")
+	testingpkg.SimpleAssert(t, queryInfo.WhereExpression_.Right_.(*BinaryOpExpression).Left_.(*types.Value).ToFloat() == 10.5)
 }
