416dc2f5f8a54851de62e3d09e41b3367108c504
Ryo Kanbayashi <ryo.contact@gmail.com>
2022-06-13 23:17:54 +0900

    implementing unit tests of AST processing: wrote TestLimitOffsetSelectQuery testcase and it passed.

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -204,3 +204,25 @@ func TestAggFuncSelectQuery(t *testing.T) {
 	testingpkg.SimpleAssert(t, *queryInfo.WhereExpression_.Left_.(*BinaryOpExpression).Left_.(*string) == "a")
 	testingpkg.SimpleAssert(t, queryInfo.WhereExpression_.Right_.(*BinaryOpExpression).Left_.(*types.Value).ToInteger() == 10)
 }
+
+func TestLimitOffsetSelectQuery(t *testing.T) {
+	sqlStr := "SELECT a, b FROM t WHERE a = 10 LIMIT 100 OFFSET 200;"
+	queryInfo := ProcessSQLStr(&sqlStr)
+	testingpkg.SimpleAssert(t, *queryInfo.QueryType_ == SELECT)
+	testingpkg.SimpleAssert(t, *queryInfo.SelectFields_[0].ColName_ == "a")
+	testingpkg.SimpleAssert(t, *queryInfo.SelectFields_[1].ColName_ == "b")
+	testingpkg.SimpleAssert(t, *queryInfo.JoinTables_[0] == "t")
+
+	testingpkg.SimpleAssert(t, queryInfo.WhereExpression_.ComparisonOperationType_ == expression.Equal)
+	testingpkg.SimpleAssert(t, queryInfo.WhereExpression_.LogicalOperationType_ == -1)
+	testingpkg.SimpleAssert(t, *queryInfo.WhereExpression_.Left_.(*BinaryOpExpression).Left_.(*string) == "a")
+	testingpkg.SimpleAssert(t, queryInfo.WhereExpression_.Right_.(*BinaryOpExpression).Left_.(*types.Value).ToInteger() == 10)
+
+	testingpkg.SimpleAssert(t, queryInfo.LimitNum_ == 100)
+	testingpkg.SimpleAssert(t, queryInfo.OffsetNum_ == 200)
+
+	sqlStr = "SELECT a, b FROM t WHERE a = 10;"
+	queryInfo = ProcessSQLStr(&sqlStr)
+	testingpkg.SimpleAssert(t, queryInfo.LimitNum_ == -1)
+	testingpkg.SimpleAssert(t, queryInfo.OffsetNum_ == -1)
+}
