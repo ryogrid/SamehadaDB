0e3288475b934f57808e06355791bf8f6e7cf180
Ryo Kanbayashi <ryo.contact@gmail.com>
2022-06-13 21:04:06 +0900

    implementing unit tests of AST processing: wrote TestAggFuncSelectQuery testcase and it passed.

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -2,6 +2,7 @@ package parser
 
 import (
 	"github.com/ryogrid/SamehadaDB/execution/expression"
+	"github.com/ryogrid/SamehadaDB/execution/plans"
 	testingpkg "github.com/ryogrid/SamehadaDB/testing"
 	"github.com/ryogrid/SamehadaDB/types"
 	"testing"
@@ -169,6 +170,34 @@ func TestWildcardSelectQuery(t *testing.T) {
 	queryInfo := ProcessSQLStr(&sqlStr)
 	testingpkg.SimpleAssert(t, *queryInfo.QueryType_ == SELECT)
 	testingpkg.SimpleAssert(t, *queryInfo.SelectFields_[0].ColName_ == "*")
+	testingpkg.SimpleAssert(t, *queryInfo.JoinTables_[0] == "t")
+	testingpkg.SimpleAssert(t, queryInfo.WhereExpression_.ComparisonOperationType_ == expression.Equal)
+	testingpkg.SimpleAssert(t, queryInfo.WhereExpression_.LogicalOperationType_ == -1)
+	testingpkg.SimpleAssert(t, *queryInfo.WhereExpression_.Left_.(*BinaryOpExpression).Left_.(*string) == "a")
+	testingpkg.SimpleAssert(t, queryInfo.WhereExpression_.Right_.(*BinaryOpExpression).Left_.(*types.Value).ToInteger() == 10)
+}
+
+func TestAggFuncSelectQuery(t *testing.T) {
+	sqlStr := "SELECT count(*), max(b), min(c), sum(d), b FROM t WHERE a = 10;"
+	queryInfo := ProcessSQLStr(&sqlStr)
+	testingpkg.SimpleAssert(t, *queryInfo.QueryType_ == SELECT)
+
+	testingpkg.SimpleAssert(t, queryInfo.SelectFields_[0].IsAgg_ == true)
+	testingpkg.SimpleAssert(t, queryInfo.SelectFields_[0].AggType_ == plans.COUNT_AGGREGATE)
+	testingpkg.SimpleAssert(t, *queryInfo.SelectFields_[0].ColName_ == "*")
+	testingpkg.SimpleAssert(t, queryInfo.SelectFields_[1].IsAgg_ == true)
+	testingpkg.SimpleAssert(t, queryInfo.SelectFields_[1].AggType_ == plans.MAX_AGGREGATE)
+	testingpkg.SimpleAssert(t, *queryInfo.SelectFields_[1].ColName_ == "b")
+	testingpkg.SimpleAssert(t, queryInfo.SelectFields_[2].IsAgg_ == true)
+	testingpkg.SimpleAssert(t, queryInfo.SelectFields_[2].AggType_ == plans.MIN_AGGREGATE)
+	testingpkg.SimpleAssert(t, *queryInfo.SelectFields_[2].ColName_ == "c")
+	testingpkg.SimpleAssert(t, queryInfo.SelectFields_[3].IsAgg_ == true)
+	testingpkg.SimpleAssert(t, queryInfo.SelectFields_[3].AggType_ == plans.SUM_AGGREGATE)
+	testingpkg.SimpleAssert(t, *queryInfo.SelectFields_[3].ColName_ == "d")
+	testingpkg.SimpleAssert(t, queryInfo.SelectFields_[4].IsAgg_ == false)
+	testingpkg.SimpleAssert(t, queryInfo.SelectFields_[4].AggType_ == 0)
+	testingpkg.SimpleAssert(t, *queryInfo.SelectFields_[4].ColName_ == "b")
+
 	testingpkg.SimpleAssert(t, *queryInfo.JoinTables_[0] == "t")
 	testingpkg.SimpleAssert(t, queryInfo.WhereExpression_.ComparisonOperationType_ == expression.Equal)
 	testingpkg.SimpleAssert(t, queryInfo.WhereExpression_.LogicalOperationType_ == -1)
