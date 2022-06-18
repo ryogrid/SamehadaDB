571083fdff3d17c94e73ddc5ead3a43bc6d19740
Ryo Kanbayashi <ryo.contact@gmail.com>
2022-06-18 23:44:33 +0900

    impl of Planner: resolved cyclic import problem.

--- catalog/catalog_test/table_catalog_reload_test.go
+++ catalog/catalog_test/table_catalog_reload_test.go
@@ -1,19 +1,19 @@
 // this code is from https://github.com/brunocalza/go-bustub
 // there is license and copyright notice in licenses/go-bustub dir
 
-package catalog
+package catalog_test
 
 import (
 	"fmt"
+	"github.com/ryogrid/SamehadaDB/catalog"
 	"github.com/ryogrid/SamehadaDB/samehada"
-	"os"
-	"testing"
-
 	"github.com/ryogrid/SamehadaDB/storage/buffer"
 	"github.com/ryogrid/SamehadaDB/storage/table/column"
 	"github.com/ryogrid/SamehadaDB/storage/table/schema"
 	testingpkg "github.com/ryogrid/SamehadaDB/testing"
 	"github.com/ryogrid/SamehadaDB/types"
+	"os"
+	"testing"
 )
 
 // test reloading serialized catalog info in db file at lauching system
@@ -25,7 +25,7 @@ func TestTableCatalogReload(t *testing.T) {
 	bpm := buffer.NewBufferPoolManager(uint32(32), samehada_instance.GetDiskManager(), samehada_instance.GetLogManager())
 
 	txn := samehada_instance.GetTransactionManager().Begin(nil)
-	catalog_old := BootstrapCatalog(bpm, samehada_instance.GetLogManager(), samehada_instance.GetLockManager(), txn)
+	catalog_old := catalog.BootstrapCatalog(bpm, samehada_instance.GetLogManager(), samehada_instance.GetLockManager(), txn)
 
 	columnA := column.NewColumn("a", types.Integer, false, nil)
 	columnB := column.NewColumn("b", types.Integer, true, nil)
@@ -39,7 +39,7 @@ func TestTableCatalogReload(t *testing.T) {
 	samehada_instance_new := samehada.NewSamehadaInstanceForTesting()
 	txn_new := samehada_instance_new.GetTransactionManager().Begin(nil)
 	//catalog := GetCatalog(bpm)
-	catalog_recov := RecoveryCatalogFromCatalogPage(samehada_instance_new.GetBufferPoolManager(), samehada_instance_new.GetLogManager(), samehada_instance_new.GetLockManager(), txn_new)
+	catalog_recov := catalog.RecoveryCatalogFromCatalogPage(samehada_instance_new.GetBufferPoolManager(), samehada_instance_new.GetLogManager(), samehada_instance_new.GetLockManager(), txn_new)
 
 	columnToCheck := catalog_recov.GetTableByOID(1).Schema().GetColumn(1)
 
