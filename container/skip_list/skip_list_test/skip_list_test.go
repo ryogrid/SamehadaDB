341e16bec9825a2504750ecce7f958c2e1f5d4a7
Ryo Kanbayashi <ryo.contact@gmail.com>
2022-08-14 11:09:34 +0900

    fuzzer test case of SkipList worked at go1.19 darwin/amd64 (Intel Mac). test fails because input range can move to one which is not accebtable.

--- container/skip_list/skip_list_test/skip_list_test.go
+++ container/skip_list/skip_list_test/skip_list_test.go
@@ -434,93 +434,18 @@ func TestSkipListItr(t *testing.T) {
 	testingpkg.SimpleAssert(t, nodeCnt == 250)
 }
 
-//func FuzzSkipLisMixOpPageBackedOnMem(f *testing.F) {
-//	const MAX_ENTRIES = 700
-//
-//	f.Add(uint8(150), uint8(10), uint16(0))
-//	f.Add(uint8(150), uint8(10), uint16(300))
-//	f.Add(uint8(150), uint8(10), uint16(600))
-//	f.Fuzz(func(t *testing.T, opTimes uint8, skipRand uint8, initialEntryNum uint16) {
-//		os.Remove("test.db")
-//		os.Remove("test.log")
-//
-//		//fmt.Println("fuzzing test now!")
-//
-//		shi := samehada.NewSamehadaInstanceForTesting()
-//		sl := skip_list.NewSkipList(shi.GetBufferPoolManager(), types.Integer)
-//
-//		// override global rand seed (seed has been set on NewSkipList)
-//		rand.Seed(3)
-//
-//		tmpSkipRand := skipRand
-//		// skip random value series
-//		for tmpSkipRand > 0 {
-//			rand.Int31()
-//			tmpSkipRand--
-//		}
-//
-//		insVals := make([]int32, 0)
-//
-//		// initial entries
-//		// uint8 range is small...
-//		useInitialEntryNum := int(initialEntryNum)
-//		for ii := 0; ii < useInitialEntryNum; ii++ {
-//			if len(insVals) < MAX_ENTRIES {
-//				insVal := rand.Int31()
-//				insVals = append(insVals, insVal)
-//				sl.Insert(samehada_util.GetPonterOfValue(types.NewInteger(int32(insVal))), uint32(insVal))
-//				insVals = append(insVals, insVal)
-//			}
-//		}
-//
-//		removedVals := make([]int32, 0)
-//		// uint8 range is small...
-//		useOpTimes := int(opTimes * 2)
-//		for ii := 0; ii < useOpTimes; ii++ {
-//			// get 0-2
-//			opType := rand.Intn(3)
-//			switch opType {
-//			case 0: // Insert
-//				if len(insVals) < MAX_ENTRIES {
-//					insVal := rand.Int31()
-//					insVals = append(insVals, insVal)
-//					sl.Insert(samehada_util.GetPonterOfValue(types.NewInteger(int32(insVal))), uint32(insVal))
-//					insVals = append(insVals, insVal)
-//				}
-//			case 1: // Delete
-//				// get 0-5 value
-//				tmpRand := rand.Intn(6)
-//				if tmpRand == 0 {
-//					// 20% is Remove to not existing entry
-//					tmpIdx := int(math.Abs(float64(rand.Intn(len(removedVals)) - 1)))
-//					tmpVal := removedVals[tmpIdx]
-//					isDeleted := sl.Remove(samehada_util.GetPonterOfValue(types.NewInteger(int32(tmpVal))), uint32(tmpVal))
-//					testingpkg.SimpleAssert(t, isDeleted == false)
-//				} else {
-//					// 80% is Remove to existing entry
-//					if len(insVals) > 0 {
-//						tmpIdx := int(math.Abs(float64(rand.Intn(len(insVals)) - 1)))
-//						isDeleted := sl.Remove(samehada_util.GetPonterOfValue(types.NewInteger(int32(insVals[tmpIdx]))), uint32(insVals[tmpIdx]))
-//						testingpkg.SimpleAssert(t, isDeleted == true)
-//						if len(insVals) == 1 {
-//							// make empty
-//							insVals = make([]int32, 0)
-//						} else if len(insVals) == tmpIdx+1 {
-//							insVals = insVals[:len(insVals)-1]
-//						} else {
-//							insVals = append(insVals[:tmpIdx], insVals[tmpIdx+1:]...)
-//						}
-//					}
-//				}
-//			case 2: // Get
-//				if len(insVals) > 0 {
-//					tmpIdx := int(math.Abs(float64(rand.Intn(len(insVals)) - 1)))
-//					gotVal := sl.GetValue(samehada_util.GetPonterOfValue(types.NewInteger(int32(insVals[tmpIdx]))))
-//					testingpkg.SimpleAssert(t, gotVal == uint32(insVals[tmpIdx]))
-//				}
-//			}
-//		}
-//	})
+func FuzzSkipLisMix(f *testing.F) {
+	os.Remove("test.db")
+	os.Remove("test.log")
+
+	shi := samehada.NewSamehadaInstance("test", 10*1024) // buffer is about 40MB
+	bpm = shi.GetBufferPoolManager()
+
+	f.Add(int32(100), uint8(150), uint8(10), uint16(300))
+	f.Fuzz(func(t *testing.T, bulkSize int32, opTimes uint8, skipRand uint8, initialEntryNum uint16) {
+		testSkipLisMix(t, bulkSize, opTimes, skipRand, initialEntryNum)
+	})
+}
 
 const MAX_ENTRIES = 700
 
@@ -529,7 +454,7 @@ var removedEntriesNum int32 = 0
 var insVals []int32
 var removedVals []int32
 
-//for debug
+// for debug
 func isAlreadyRemoved(checkVal int32) bool {
 	for _, val := range removedVals {
 		if val == checkVal {
