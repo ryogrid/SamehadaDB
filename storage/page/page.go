669ed68805be1c1bcb2bea7d4515797502dd24ee
Ryo Kanbayashi <ryo.contact@gmail.com>
2022-09-05 19:48:31 +0900

    making SkipList concurrent: debugging TestSkipListParallelSimple2Integer test case fail. changed RWMutex which is used at Page::New to one for debug too.

--- storage/page/page.go
+++ storage/page/page.go
@@ -78,10 +78,10 @@ func (p *Page) Copy(offset uint32, data []byte) {
 
 // New creates a new page
 func New(id types.PageID, isDirty bool, data *[common.PageSize]byte) *Page {
-	return &Page{id, uint32(1), isDirty, data, common.NewRWLatch()}
+	//return &Page{id, uint32(1), isDirty, data, common.NewRWLatch()}
 
-	//// TODO: (SDB) customized RWMutex for concurrent skip list debug
-	//return &Page{id, uint32(1), isDirty, data, common.NewRWLatchDebug()}
+	// TODO: (SDB) customized RWMutex for concurrent skip list debug
+	return &Page{id, uint32(1), isDirty, data, common.NewRWLatchDebug()}
 }
 
 // New creates a new empty page
