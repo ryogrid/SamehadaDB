708929ac6380484abeb83ac9e11d60cc6ff53475
Ryo Kanbayashi <ryo.contact@gmail.com>
2023-02-05 17:28:42 +0900

    random str generator's min length is changed from 50 to 1 and the test passed. (seed=13)

--- samehada/samehada_util/samehada_util.go
+++ samehada/samehada_util/samehada_util.go
@@ -82,16 +82,16 @@ func GetPonterOfValue(value types.Value) *types.Value {
 	return &val
 }
 
-// min length is 50
+// min length is 1
 func GetRandomStr(maxLength int32) *string {
 	alphabets :=
 		"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!#$%&'(),-./:;<=>?@[]^_`{|}~"
 	//"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 	var len_ int
-	for len_ < 50 {
-		len_ = 1 + (rand.Intn(math.MaxInt32))%(int(maxLength)-1)
-	}
-	//len_ = 1 + (rand.Intn(math.MaxInt32))%(int(maxLength)-1)
+	//for len_ < 50 {
+	//	len_ = 1 + (rand.Intn(math.MaxInt32))%(int(maxLength)-1)
+	//}
+	len_ = 1 + (rand.Intn(math.MaxInt32))%(int(maxLength)-1)
 
 	s := ""
 	for j := 0; j < len_; j++ {
