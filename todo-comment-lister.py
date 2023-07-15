bde4a2cdc5e5bd8cb60a4ec4b84edbb505153586
Ryo Kanbayashi <ryo.contact@gmail.com>
2023-07-15 21:42:48 +0900

    added todo-comment-lister.py

---
+++ todo-comment-lister.py
@@ -0,0 +1,27 @@
+# coding: utf-8
+import os, sys, io
+
+def listUpTodoComments():
+    for root, dirs, files in os.walk(top='./'):
+        for file in files:
+            filePath = os.path.join(root, file)
+            filePathToPrint = filePath.replace('\\', '/')
+            isPathPrinted = False
+            if filePath.endswith('.go'):
+                with open(filePath, mode='r', encoding='utf-8') as f:
+                    lineNum = 1
+                    for line in f:
+                        if line.find('TODO') > -1 and line.find('(SDB)') > -1:
+                            if not isPathPrinted:
+                                print(filePathToPrint)
+                                isPathPrinted = True
+                            stripedLine = line.strip()
+                            print('  ' + '{:<5}'.format(str(lineNum) + ':') + ' ' + stripedLine)
+                        lineNum += 1
+
+def main():
+    sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+    sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding='utf-8')
+    listUpTodoComments()
+
+main()
