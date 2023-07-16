8b2bef3c69f4175e8be25fe56f4c867203cea39c
Ryo Kanbayashi <ryo.contact@gmail.com>
2023-07-16 12:58:20 +0900

    refactoring TODO comments: wip. (2)

--- todo-comment-lister.py
+++ todo-comment-lister.py
@@ -1,6 +1,8 @@
 # coding: utf-8
 import os, sys, io
 
+additonalFilter = None
+
 def listTodoComments():
     for root, dirs, files in os.walk(top='./'):
         for file in files:
@@ -11,17 +13,29 @@ def listTodoComments():
                 with open(filePath, mode='r', encoding='utf-8') as f:
                     lineNum = 1
                     for line in f:
-                        if line.find('TODO') > -1 and line.find('(SDB)') > -1 and line.find('[OPT]') > -1:
-                            if not isPathPrinted:
-                                print(filePathToPrint)
-                                isPathPrinted = True
-                            stripedLine = line.strip()
-                            print('  ' + '{:<5}'.format(str(lineNum) + ':') + ' ' + stripedLine)
-                        lineNum += 1
+                        try:
+                            if line.find('TODO') > -1 and line.find('(SDB)') > -1 and line.find('[OPT]') > -1:
+                                if additonalFilter and line.find(additonalFilter) == -1:
+                                    continue
+
+                                if not isPathPrinted:
+                                    print(filePathToPrint)
+                                    isPathPrinted = True
+                                stripedLine = line.strip()
+                                print('  ' + '{:<5}'.format(str(lineNum) + ':') + ' ' + stripedLine)
+                        finally:
+                            lineNum += 1
 
 def main():
+    global additonalFilter
+
     sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
     sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding='utf-8')
+
+    # additonal filtering option: -f "<filtering string>"
+    if len(sys.argv) == 3 and sys.argv[1] == '-f':
+        additonalFilter = sys.argv[2]
+
     listTodoComments()
 
 main()
