ec55bbebb2b0a9e0b372e6e99ce9ff4255bc1aa0
Ryo Kanbayashi <ryo.contact@gmail.com>
2023-07-23 16:01:59 +0900

    implementing Index Join: wip5.

--- execution/plans/projection.go
+++ execution/plans/projection.go
@@ -4,25 +4,16 @@ import (
 	"github.com/ryogrid/SamehadaDB/storage/table/schema"
 )
 
-// TODO: (SDB) [OPT] not implemented yet (projection.go)
-
 type ProjectionPlanNode struct {
 	*AbstractPlanNode
-	//projectionColumns []*parser.SelectFieldExpression
-	projectionColumns *schema.Schema
 }
 
-func NewProjectionPlanNode(child Plan, selectColumns *schema.Schema) Plan {
-	childOutSchema := child.OutputSchema()
-	return &ProjectionPlanNode{&AbstractPlanNode{childOutSchema, []Plan{child}}, selectColumns}
+func NewProjectionPlanNode(child Plan, projectColumns *schema.Schema) Plan {
+	return &ProjectionPlanNode{&AbstractPlanNode{projectColumns, []Plan{child}}}
 }
 
 func (p *ProjectionPlanNode) GetType() PlanType {
 	return Projection
-}
-
-func (p *ProjectionPlanNode) GetProjectionColumns() *schema.Schema {
-	return p.projectionColumns
 }
 
 func (p *ProjectionPlanNode) GetTableOID() uint32 {
