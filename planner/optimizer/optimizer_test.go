a1caa1a58018ac19163ba2deb3ec1c839421e3d2
Ryo Kanbayashi <ryo.contact@gmail.com>
2023-08-18 16:10:55 +0900

    refactored optimizer_test.go .

--- planner/optimizer/optimizer_test.go
+++ planner/optimizer/optimizer_test.go
@@ -1,52 +1,68 @@
 package optimizer
 
-/*
+import (
+	"fmt"
+	"github.com/ryogrid/SamehadaDB/catalog"
+	"github.com/ryogrid/SamehadaDB/common"
+	"github.com/ryogrid/SamehadaDB/execution/executors"
+	"github.com/ryogrid/SamehadaDB/parser"
+	"github.com/ryogrid/SamehadaDB/recovery"
+	"github.com/ryogrid/SamehadaDB/storage/access"
+	"github.com/ryogrid/SamehadaDB/storage/buffer"
+	"github.com/ryogrid/SamehadaDB/storage/disk"
+	testingpkg "github.com/ryogrid/SamehadaDB/testing"
+	"testing"
+)
+
+func setupTablesAndStatisticsDataForTesting(c *catalog.Catalog, exec_ctx *executors.ExecutorContext, txn *access.Transaction) {
+	// TODO: (SDB) [OPT] not implemented yet (setupTablesAndStatisticsDataForTesting)
+}
+
 func TestSimplePlanOptimization(t *testing.T) {
-	// TODO: (SDB) [OPT] not implemented yet (TestSimplePlanOptimization)
+	diskManager := disk.NewDiskManagerTest()
+	defer diskManager.ShutDown()
+	log_mgr := recovery.NewLogManager(&diskManager)
+	log_mgr.ActivateLogging()
+	testingpkg.Assert(t, log_mgr.IsEnabledLogging(), "")
+	fmt.Println("System logging is active.")
+	bpm := buffer.NewBufferPoolManager(common.BufferPoolMaxFrameNumForTest, diskManager, log_mgr) //, recovery.NewLogManager(diskManager), access.NewLockManager(access.REGULAR, access.PREVENTION))
+	txn_mgr := access.NewTransactionManager(access.NewLockManager(access.REGULAR, access.DETECTION), log_mgr)
+
+	txn := txn_mgr.Begin(nil)
+	c := catalog.BootstrapCatalog(bpm, log_mgr, access.NewLockManager(access.REGULAR, access.PREVENTION), txn)
+	exec_ctx := executors.NewExecutorContext(c, bpm, txn)
+
+	setupTablesAndStatisticsDataForTesting(c, exec_ctx, txn)
+
+	// TODO: (SDB) [OPT] need to write query for testing BestJoin func (TestSimplePlanOptimization)
+	queryStr := "TO BE WRITTEN"
+	queryInfo := parser.ProcessSQLStr(&queryStr)
+
+	optimalPlans := NewSelingerOptimizer().findBestScans(queryInfo, exec_ctx, c, txn)
+	solution := NewSelingerOptimizer().findBestJoin(optimalPlans, queryInfo, exec_ctx, c, txn)
+	fmt.Println(solution)
 }
 
 func TestBestScan(t *testing.T) {
-	shi := samehada.NewSamehadaInstance(t.Name(), common.BufferPoolMaxFrameNumForTest)
-	shi.GetLogManager().ActivateLogging()
-	testingpkg.Assert(t, shi.GetLogManager().IsEnabledLogging(), "")
+	diskManager := disk.NewDiskManagerTest()
+	defer diskManager.ShutDown()
+	log_mgr := recovery.NewLogManager(&diskManager)
+	log_mgr.ActivateLogging()
+	testingpkg.Assert(t, log_mgr.IsEnabledLogging(), "")
 	fmt.Println("System logging is active.")
+	bpm := buffer.NewBufferPoolManager(common.BufferPoolMaxFrameNumForTest, diskManager, log_mgr) //, recovery.NewLogManager(diskManager), access.NewLockManager(access.REGULAR, access.PREVENTION))
+	txn_mgr := access.NewTransactionManager(access.NewLockManager(access.REGULAR, access.DETECTION), log_mgr)
 
-	txn_mgr := shi.GetTransactionManager()
 	txn := txn_mgr.Begin(nil)
+	c := catalog.BootstrapCatalog(bpm, log_mgr, access.NewLockManager(access.REGULAR, access.PREVENTION), txn)
+	exec_ctx := executors.NewExecutorContext(c, bpm, txn)
 
-	c := catalog.BootstrapCatalog(shi.GetBufferPoolManager(), shi.GetLogManager(), shi.GetLockManager(), txn)
-	exec_ctx := executors.NewExecutorContext(c, shi.GetBufferPoolManager(), txn)
+	setupTablesAndStatisticsDataForTesting(c, exec_ctx, txn)
 
-	// TODO: (SDB) [OPT] need to setup tables for use in query (TestBestScan)
-	// TODO: (SDB) [OPT] need to setup of statistics data of created tables and query which uses the tables for testing BestJoin func (TestBestScan)
-	//table_info, _ := executors.GenerateTestTabls(c, exec_ctx, txn)
-	query := new(parser.QueryInfo)
+	// TODO: (SDB) [OPT] need to write query for testing BestJoin func (TestSimplePlanOptimization)
+	queryStr := "TO BE WRITTEN"
+	queryInfo := parser.ProcessSQLStr(&queryStr)
 
-	optimalPlans := NewSelingerOptimizer().findBestScans(query, exec_ctx, c, txn)
-	testingpkg.Assert(t, len(optimalPlans) == len(query.JoinTables_), "len(optimalPlans) != len(query.JoinTables_)")
+	optimalPlans := NewSelingerOptimizer().findBestScans(queryInfo, exec_ctx, c, txn)
+	testingpkg.Assert(t, len(optimalPlans) == len(queryInfo.JoinTables_), "len(optimalPlans) != len(query.JoinTables_)")
 }
-
-func TestBestJoin(t *testing.T) {
-	shi := samehada.NewSamehadaInstance(t.Name(), common.BufferPoolMaxFrameNumForTest)
-	shi.GetLogManager().ActivateLogging()
-	testingpkg.Assert(t, shi.GetLogManager().IsEnabledLogging(), "")
-	fmt.Println("System logging is active.")
-
-	txn_mgr := shi.GetTransactionManager()
-	txn := txn_mgr.Begin(nil)
-
-	c := catalog.BootstrapCatalog(shi.GetBufferPoolManager(), shi.GetLogManager(), shi.GetLockManager(), txn)
-	exec_ctx := executors.NewExecutorContext(c, shi.GetBufferPoolManager(), txn)
-
-	// TODO: (SDB) [OPT] need to create tables for use in query (TestBestJoin)
-	// TODO: (SDB) [OPT] need to setup of statistics data of created tables and query which uses the tables for testing BestJoin func (TestBestJoin)
-	//table_info, _ := executors.GenerateTestTabls(c, exec_ctx, txn)
-	query := new(parser.QueryInfo)
-
-	// TODO: (SDB) [OPT] need to setup of optimalPlans (TestBestJoin)
-	optimalPlans := make(map[mapset.Set[string]]CostAndPlan, 0)
-
-	solution := NewSelingerOptimizer().findBestJoin(optimalPlans, query, exec_ctx, c, txn)
-	fmt.Println(solution)
-}
-*/
