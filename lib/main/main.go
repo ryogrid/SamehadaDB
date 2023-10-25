39be9da923a580c6784f30594a4951fd0dc4708c
Ryo Kanbayashi <ryo.contact@gmail.com>
2023-10-25 09:15:28 +0900

    removed lib/main/main.go because it is needed on library.

--- lib/main/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-// current SamehadaDB can be used as an embedded DB form only.
-// so, this entry point is used for running snippet code for debugging now...
-func main() {
-	panic("SamehadaDB can not be use as executable!")
-}
