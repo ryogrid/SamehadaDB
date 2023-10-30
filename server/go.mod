83f454c869e8ce9e41d899edea356ca20b164b38
Ryo Kanbayashi <ryo.contact@gmail.com>
2023-10-30 21:59:44 +0900

    trying msgpack transport: WIP

--- server/go.mod
+++ server/go.mod
@@ -7,6 +7,7 @@ go 1.18
 require (
 	github.com/ant0ine/go-json-rest v3.3.2+incompatible
 	github.com/ryogrid/SamehadaDB/lib v0.0.0-00010101000000-000000000000
+	github.com/ugorji/go/codec v1.1.7
 )
 
 require (
--- server/main.go
+++ server/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"github.com/ant0ine/go-json-rest/rest"
 	"github.com/ryogrid/SamehadaDB/lib/samehada"
 	"github.com/ryogrid/SamehadaDB/server/signal_handle"
+	"github.com/ugorji/go/codec"
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -62,6 +65,56 @@ func postQuery(w rest.ResponseWriter, req *rest.Request) {
 	})
 }
 
+func postQueryMsgPack(w rest.ResponseWriter, req *rest.Request) {
+	if signal_handle.IsStopped {
+		http.Error(w.(http.ResponseWriter), "Server is stopped", http.StatusGone)
+		return
+	}
+
+	input := QueryInput{}
+	err := req.DecodeJsonPayload(&input)
+
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w.(http.ResponseWriter), err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if input.Query == "" {
+		http.Error(w.(http.ResponseWriter), "Query is required", 400)
+		return
+	}
+
+	err2, results := db.ExecuteSQL(input.Query)
+	if err2 != nil {
+		http.Error(w.(http.ResponseWriter), err2.Error(), http.StatusBadRequest)
+		return
+	}
+
+	rows := make([]Row, 0)
+	for _, row := range results {
+		rows = append(rows, Row{row})
+	}
+
+	var w_ io.Writer = new(bytes.Buffer)
+	var h codec.Handle = new(codec.MsgpackHandle)
+	var enc = codec.NewEncoder(w_, h)
+	var err3 = enc.Encode(rows)
+	if err3 != nil {
+		fmt.Println(err3)
+		http.Error(w.(http.ResponseWriter), err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	dec := codec.NewDecoder(w_.(*bytes.Buffer), h)
+	var decoded interface{}
+	dec.Decode(decoded)
+	fmt.Println(decoded)
+
+	w.Header().Set("Content-Type", "application/octent-stream")
+	w.(http.ResponseWriter).Write(w_.(*bytes.Buffer).Bytes())
+}
+
 func launchDBAndListen() {
 	api := rest.NewApi()
 
@@ -83,6 +136,7 @@ func launchDBAndListen() {
 
 	router, err := rest.MakeRouter(
 		&rest.Route{"POST", "/Query", postQuery},
+		&rest.Route{"POST", "/QueryMsgPack", postQueryMsgPack},
 	)
 	if err != nil {
 		log.Fatal(err)
